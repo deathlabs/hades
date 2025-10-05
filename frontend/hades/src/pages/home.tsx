1151fea9e89590954ebd558e9b8e292d3a9fa9dd
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2025-10-04 21:51:02 -0400

    Updated the splash page (#4)
    * Updated the introduction
    
    * Updated the splash page
    
    ---------
    
    Co-authored-by: cyberphor <victor.fernandeziii@gmail.com>

--- frontend/hades/src/pages/home.tsx
+++ frontend/hades/src/pages/home.tsx
@@ -1,14 +1,9 @@
-import Typography from '@mui/material/Typography';
 import { PageContainer } from '@toolpad/core/PageContainer';
 
-export default function Home() {
-  
 
+export default function Home() {
   return (    
     <PageContainer>
-      <Typography>
-        Welcome to HADES!
-      </Typography>
     </PageContainer>
   );
 }
