a866982ca9b3ba3b5f41f6577c9c2d765a5b87e1
cyberphor <victor.fernandeziii@gmail.com>
2025-10-05 21:57:19 -0700

    Updated frontend form

--- backend/hades/tasks/preparation.py
+++ backend/hades/tasks/preparation.py
@@ -8,7 +8,7 @@ def get_scenario(
     sender: HadesAgent, recipient: HadesAgent, scenario: dict
 ) -> dict:
     # TODO: add code to include all fields of the scenario in the message (description, RoE, etc.)
-    message = f"Your team is conducting Cyber Adversary Emulation within an on-prem computing environment authorized for detection engineering and incident response training. Your IP address is {scenario['address']}."
+    message = f"Your team is responsible for conducting Cyber Adversary Emulation in a computing environment authorized for detection engineering and incident response training. Your IP address is {scenario['address']}. You are allowed to use the following techniques: {", ".join(scenario['allowed'])}. You are not authorized to peform the following techniques: {", ".join(scenario['prohibited'])}."
     return {
         "sender": sender,
         "recipient": recipient,
